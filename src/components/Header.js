1a7be4b6e61dee25c0b8d862ab4494c1093786c0
Rob Rutjes <450901@student.fontys.nl>
2022-01-14 09:36:08 +0100

    Feat - Add login/register with Auth0
    Closes #3 US-01 | Als (nieuwe) gebruiker, wil ik mijzelf kunnen registeren, zodat ik gebruik kan maken van de applicatie, Closes #4 US-02 | Als gebruiker, wil ik kunnen inloggen, zodat ik gebruik kan maken van de applicatie, Closes #5 US-03 | Als gebruiker, wil ik kunnen uitloggen, zodat ik de applicatie veilig kan afsluiten

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
+    const { loginWithRedirect, logout, user } = useAuth0();
+
     return (
         <div className="ui fixed menu">
             <div className="ui container center">
                 <h2>Products</h2>
+
+                <button onClick={() => loginWithRedirect()} className="ui button blue center"> Login </button>
+                <button onClick={() => logout()} className="ui button blue center"> Logout </button>
             </div>
         </div>
     );
